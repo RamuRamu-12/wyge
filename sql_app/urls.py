dddba76cb81e388425c0d8da6dca23fce7b0ebf1
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2024-12-14 15:53:10 +0530

    Delete sql_app directory

--- sql_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_excel, name='upload'),
-    path('query/', views.execute_query_view, name='query'),
-]
