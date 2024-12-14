f39450dc914a4d8961f759f12081b9290aff2336
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2024-12-14 15:50:06 +0530

    Delete agi_agents_backend directory

--- agi_agents_backend/asgi.py
+++
@@ -1,9 +0,0 @@
-"""
-ASGI config for agi_agents_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
